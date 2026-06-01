f449462849ac9652939f2575caceaba09cd9eec5
leju-Steven <steven@leju.com.tw>
2026-06-01 23:55:14 +0800

    重構 MessagesRepository 類別，更新 findOne 和 findAll 方法以使用 MessageMap 類型，並實作 create 方法以新增訊息

--- src/messages/messages.repository.ts
+++ src/messages/messages.repository.ts
@@ -1,14 +1,39 @@
 import { readFile, writeFile } from 'fs/promises';
 
+type Message = {
+  id: string;
+  message: string;
+};
+
+type MessageMap = Record<string, Message>;
+
 export class MessagesRepository {
   async findOne(id: string) {
     const contents = await readFile('messages.json', 'utf-8');
-    const messages = JSON.parse(contents) as Record<string, string>;
+    const messages = JSON.parse(contents) as MessageMap;
 
     return messages[id];
   }
 
-  async findAll() {}
+  async findAll() {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents) as MessageMap;
 
-  async create(message: string) {}
+    return messages;
+  }
+
+  async create(message: string) {
+    const contents = await readFile('messages.json', 'utf-8');
+    const messages = JSON.parse(contents) as MessageMap;
+
+    const id = Math.random().toString(36).substring(2, 5);
+    messages[id] = {
+      id,
+      message,
+    };
+
+    await writeFile('messages.json', JSON.stringify(messages));
+
+    return id;
+  }
 }
